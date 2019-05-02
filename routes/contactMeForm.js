e60143e7a95d3af251d3faf7671fd5b94af7e98c
Neota Moe <neota.moe@gmail.com>
2019-05-02 18:17:22 -0500

    more config vars

--- routes/contactMeForm.js
+++ routes/contactMeForm.js
@@ -7,6 +7,8 @@ var nodemailer = require('nodemailer');
 // var password = require('../password');  // need for local dev, causes errors when deploy to heroku
 
 var secretPassword = process.env.PASSWORD || password.password;
+var toEmail = process.env.TOEMAIL || password.toEmail;
+var userEmail = process.env.USEREMAIL || password.userEmail;
 
 router.post('/', function(req,res){
   console.log('req.body:', req.body);
@@ -16,14 +18,14 @@ router.post('/', function(req,res){
       port: 465,
       secure: true, // secure:true for port 465, secure:false for port 587
       auth: {
-          user: 'science123app@gmail.com',
+          user: userEmail,
           pass: secretPassword
       }
   });
   // this is email to be sent to admin
   var mailOptions = {
     from: '"'+req.body.name+'"<'+ req.body.email+'>',
-    to: '"neotamoe" <neotamoe@gmail.com>',
+    to: '"neotamoe" <' + toEmail +'>',
     subject: 'from neotamoe.com contact me form',
     text: req.body.name + ' (email: ' + req.body.email + ') sent a message from neotamoe.com.  ' + req.body.comments, // plain text body  // insert content from form here
     html: '<p>'+ req.body.name + ' (email: ' + req.body.email + ') sent a message from neotamoe.com.  </p><p>' + req.body.comments +'</p>'// html body
